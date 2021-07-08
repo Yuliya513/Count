2fa10dffbd1b3c7e010c69e30414d55a5898c97b
Yuliya513 <83278354+Yuliya513@users.noreply.github.com>
2021-07-08 14:31:33 +0300

    Debugging process

---
+++ src/com/company/CountCharacter.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class PrintStack {
+    void methodA() throw;
+
+    FileNotFoundException {
+        throw new FileNotFoundException();
+    }
+
+    public static void main(String[] args) {
+
+        int CountCharacteristics = 0;
+        String character = '';
+        BufferReader br = new BufferReader();
+        new InputStreamReader(System in);
+        System.out.println("Input word");
+        test Word = br.readLine();
+        System.out.println("Input character");
+        character = br.readline();
+        for (int i = 0; i < testWord.length(),i++)
+
+        {
+            character = testWord.sub(1, 1)
+            if (character.equals(character)) countCharacter
+
+            System.out.println("There are character" + character + " " + countCharacters + "times in the word" + testWord);
+        }
+    }
+}
+
